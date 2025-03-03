ca12b43865a5fd1fb2864e1164c64d29deda6893
vvtikhomir <vvtikhomirmts.ru>
2025-03-03 14:48:56 +0300

    wrapper component

---
+++ src/components/wrapper/wrapper.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './wrapper.scss'
+
+type WrapperProps = {
+  className?: string;
+  direction?: 'row' | 'column';
+  justify?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around';
+  align?: 'flex-start' | 'flex-end' | 'center' | 'stretch';
+  gap?: string;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+export const Wrapper: React.FC<React.PropsWithChildren<WrapperProps>> = ({
+  children,
+  className = '',
+  direction = 'column',
+  justify = '',
+  align = '',
+  gap = '0px',
+  ...rest
+}) => {
+
+  const wrapperStyles = `
+    display: flex;
+    flex-direction: ${direction};
+    justify-content: ${justify};
+    align-items: ${align};
+    gap: ${gap};
+  `.trim();
+
+  return (
+    <div
+      className={`wrapper ${className}`}
+      style={{ ...rest.style, ...(CSS.supports('display', 'flex') && { cssText: wrapperStyles }) }}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+};
