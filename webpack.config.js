e5946f5ea653b13d37ba1e020ced94af680188b2
Александр Тимофеев <aandreevtimofeev@sberbank.ru>
2025-04-07 13:12:10 +0300

    [refactor] optimize webpack cfg

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,19 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = (env, argv) => {
     const isProduction = argv.mode === 'production';
+    const isGithubPages = process.env.GITHUB_PAGES === 'true';
 
     return {
         entry: './src/index.tsx',
         output: {
             path: path.resolve(__dirname, 'dist'),
-            publicPath: '/',
-            filename: 'bundle.js',
+            publicPath: isGithubPages ? '/react-study-delivery-app/' : '/',
+            filename: isProduction ? '[name].[contenthash].js' : 'bundle.js',
             clean: true,
         },
         mode: isProduction ? 'production' : 'development',
@@ -21,6 +24,26 @@ module.exports = (env, argv) => {
             hot: true,
             historyApiFallback: true,
             port: 9000,
+        },
+        optimization: {
+            minimize: isProduction,
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        compress: {
+                            drop_console: isProduction,
+                        },
+                    },
+                }),
+                new CssMinimizerPlugin(),
+            ],
+            splitChunks: {
+                chunks: 'all',
+                name: false,
+            },
+            runtimeChunk: {
+                name: 'runtime',
+            },
         },
         module: {
             rules: [
@@ -59,9 +82,21 @@ module.exports = (env, argv) => {
                 },
                 {
                     test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                    type: 'asset/resource', // Use asset/resource for Webpack 5
+                    type: 'asset/resource',
                     generator: {
-                        filename: 'fonts/[name][ext]', // Output fonts to the 'fonts' directory
+                        filename: 'fonts/[name].[contenthash][ext]',
+                    },
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif)$/i,
+                    type: 'asset',
+                    parser: {
+                        dataUrlCondition: {
+                            maxSize: 10 * 1024, // 10kb
+                        },
+                    },
+                    generator: {
+                        filename: 'images/[name].[contenthash][ext]',
                     },
                 },
             ],
@@ -75,8 +110,22 @@ module.exports = (env, argv) => {
         plugins: [
             new HtmlWebpackPlugin({
                 template: './public/index.html',
+                minify: isProduction ? {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    removeRedundantAttributes: true,
+                    useShortDoctype: true,
+                    removeEmptyAttributes: true,
+                    removeStyleLinkTypeAttributes: true,
+                    keepClosingSlash: true,
+                    minifyJS: true,
+                    minifyCSS: true,
+                    minifyURLs: true,
+                } : false,
             }),
-            ...(isProduction ? [new MiniCssExtractPlugin({ filename: 'styles.css' })] : []),
+            ...(isProduction ? [new MiniCssExtractPlugin({ 
+                filename: 'styles.[contenthash].css' 
+            })] : []),
         ],
     };
 };
