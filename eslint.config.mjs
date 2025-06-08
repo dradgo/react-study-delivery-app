ea3cfa149af1d1c4ecbed5b2f60c74b20ae6f04f
dradgo <dradgo@gmail.com>
2025-06-08 12:19:50 +0300

    chore: add ts support to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,22 @@
 // eslint.config.mjs
 
 import prettierPlugin from "eslint-plugin-prettier";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
   {
     files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: new URL('.', import.meta.url).pathname,
+      },
+    },
     plugins: {
       prettier: prettierPlugin,
+      '@typescript-eslint': tseslint,
     },
     rules: {
       "prettier/prettier": ["error", {}, { usePrettierrc: true }]
